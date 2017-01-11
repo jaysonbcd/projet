4da8806a0e0b71fe45c44d1e13ace87cc083522f
boucaud.jayson <boucaud.jayson@gmail.com>
2017-01-11 02:39:50 -0500

    projet

--- loc.php
+++ loc.php
@@ -1,27 +1,26 @@
 <?php
-
-// Get values from the form
 $name=$_POST['name'];
 $city=$_POST['city'];
 $phone=$_POST['phone'];
 $email=$_POST['email'];
 $monmessage=$_POST['monmessage'];
  
-$to = "boucaud.jayson@gmail.com";
+$to = "votre-email@gmail.com";
 $subject = "Mon Contact Form";
 $message = " Name: " . $name . "\r\n City: " . $city . "\r\n Phone: " . $phone . "\r\n Email: " . $email . "\r\n Monmessage: " . $monmessage;
  
  
-$from = "Jayson";
+$from = "Blueseodesign";
 $headers = "From:" . $from . "\r\n";
 $headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
  
 if(@mail($to,$subject,$message,$headers))
-{
-  print "<script>document.location.href='merci.html';</script>";
-}else{
-  echo "Erreur! Veuillez Remplir les Champs Requis.";
-}
+  echo "<p style="text-align:center;font-size:18px; color:green;">
+  Votre mail est bien envoyer.</p>
+  <p style="text-align:center;font-size:18px; color:green;">
+  <a href="index.html">Cliquez ici pour retourner a l'accueil du site</a>
+  </p">
+
 
 
 ?>
